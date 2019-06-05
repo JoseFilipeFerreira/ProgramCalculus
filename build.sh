d78d6c4f67d137e6ef15aa686e003e6498d01e65
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-06-06 00:27:01 +0100

    iueghqohdklqwjhw

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 lhs2TeX cp1819t.lhs > cp1819t.tex
-pdflatex cp1819t.tex
-pdflatex cp1819t.tex
+pdflatex -interaction batchmode cp1819t.tex
+bibtex cp1819t.aux
+makeindex cp1819t.idx
+pdflatex -interaction batchmode cp1819t.tex
 rm *.tex
 stack ghci cp1819t.lhs
